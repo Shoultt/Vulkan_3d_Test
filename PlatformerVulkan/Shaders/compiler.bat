4be240fbbee7e22652c3d3e36864f052f6334cb5
shoultt <ahh.cobaka@gmail.com>
2023-12-23 14:02:53 +0500

    shaders

---
+++ PlatformerVulkan/Shaders/compiler.bat
@@ -0,0 +1,3 @@
+C:\Users\hog\source\repos\PlatformerVulkan\PlatformerVulkan\Shaders\glslc vs.vert -o vert.spv
+C:\Users\hog\source\repos\PlatformerVulkan\PlatformerVulkan\Shaders\glslc fs.frag -o frag.spv
+pause
